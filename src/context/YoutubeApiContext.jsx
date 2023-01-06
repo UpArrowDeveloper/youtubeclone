74b82d1d60698a71be80db8523945fc271d2762d
UpArrowDeveloper <investorforecastmedia@gmail.com>
2023-01-06 18:09:16 +0900

    Added video card feature

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,9 +1,13 @@
 import { createContext, useContext } from "react";
+import FakeYoutubeClient from "../api/fakeYoutubeClient";
+import YoutubeClient from "../api/youtubeClient";
 import Youtube from "../api/youtube";
 
 export const YoutubeApiContext = createContext();
 
-const youtube = new Youtube();
+// const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return (
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import VideoCard from "../components/VideoCard";
 import Youtube, { search } from "../api/youtube";
-import FakeYoutube from "../api/fakeYoutube";
+import FakeYoutube from "../api/fakeYoutubeClient";
 import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Videos() {
@@ -20,7 +20,7 @@ export default function Videos() {
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong 🙁</p>}
       {videos && (
-        <ul>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
